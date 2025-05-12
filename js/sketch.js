c90f358d3366518e90ef80169033a91bdd65ace6
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-05-13 01:15:50 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,51 +1,51 @@
-const cid = "QmTrs32nNX8U7a1aWecZSTyfL1wXFkdX1FUxyxYGgh8E3i"
-let angle = 0
-let elements = []
-let timer = 0
-let MAX_TIME = 3600
+const cid = "QmTrs32nNX8U7a1aWecZSTyfL1wXFkdX1FUxyxYGgh8E3i";
+let angle = 0;
+let elements = [];
+let timer = 0;
+let MAX_TIME = 3600;
 
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight)
-  canvas.parent('canvas-holder')
-  background(0)
-  textFont('monospace')
-  textSize(16)
-  textAlign(CENTER, CENTER)
+  const canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('canvas-holder');
+  background(0);
+  textFont('monospace');
+  textSize(16);
+  textAlign(CENTER, CENTER);
 
-fetch(`https://ipfs.io/ipfs/${cid}/hola.txt`)
+  fetch(`https://ipfs.io/ipfs/${cid}/hola.txt`)
     .then(response => response.text())
     .then(data => {
       for (let i = 0; i < data.length; i++) {
-        const char = data.charCodeAt(i)
-        const radius = random(50, width / 2)
-        const speed = random(0.005, 0.02)
-        const col = color((char * 3) % 255, (char * 5) % 255, (char * 7) % 255)
-        elements.push({ radius, speed, angleOffset: angle, color: col })
+        const char = data.charCodeAt(i);
+        const radius = random(50, width / 2);
+        const speed = random(0.005, 0.02);
+        const col = color((char * 3) % 255, (char * 5) % 255, (char * 7) % 255);
+        elements.push({ radius, speed, angleOffset: angle, color: col });
       }
     })
     .catch(err => {
-      console.error("Error al cargar desde IPFS:", err)
-    })
+      console.error("Error al cargar desde IPFS:", err);
+    });
 }
 
 function draw() {
-  background(0, 20)
-  translate(width / 2, height / 2)
+  background(0, 20);
+  translate(width / 2, height / 2);
 
   for (let el of elements) {
-    let x = el.radius * cos(angle + el.angleOffset)
-    let y = el.radius * sin(angle + el.angleOffset)
-    fill(el.color)
-    noStroke()
-    ellipse(x, y, 8)
-    el.angleOffset += el.speed
+    let x = el.radius * cos(angle + el.angleOffset);
+    let y = el.radius * sin(angle + el.angleOffset);
+    fill(el.color);
+    noStroke();
+    ellipse(x, y, 8);
+    el.angleOffset += el.speed;
   }
 
-  angle += 0.01
-  timer++
+  angle += 0.01;
+  timer++;
   if (timer > MAX_TIME) {
-    noLoop()
-    clear()
-    document.getElementById('canvas-holder').innerHTML = '<p style="color:white;text-align:center;">Tu intervención ha desaparecido.</p>'
+    noLoop();
+    clear();
+    document.getElementById('canvas-holder').innerHTML = '<p style="color:white;text-align:center;">Tu intervención ha desaparecido.</p>';
   }
 }
